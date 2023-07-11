f377a77b5c00cd11c4e4d8b3af23478531d071d7
uma_the390 <ryoma.k.0224@gmail.com>
2023-07-11 01:30:27 +0000

    fix

--- scrapbox.js
+++ scrapbox.js
@@ -1,4 +1,5 @@
 'use strict'
 
-const tabs = await chrome.tabs.getCurrent();
-console.log(tabs[0]);
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    console.log(tabs[0]);
+});
