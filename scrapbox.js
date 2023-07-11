7dc6aaee9ed2bbff779ad105b17382c41a1f4d25
uma_the390 <ryoma.k.0224@gmail.com>
2023-07-11 01:49:53 +0000

    fix

--- scrapbox.js
+++ scrapbox.js
@@ -1,7 +1,7 @@
 'use strict'
 
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    const maxLength = 20;
+    const maxLength = 50;
     let currentTab = tabs[0];
     let truncatedURL = currentTab.url.slice(0, maxLength) + '...';
 
